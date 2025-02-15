d403e8a27f6e15da54d8e334d45ed545fef85274
PRIY MAVANI <priy.mavani.cg@gmail.com>
2025-02-15 09:57:22 +0530

    Update Potter.jsx

--- src/assets/Component/Potter.jsx
+++ src/assets/Component/Potter.jsx
@@ -1,54 +1,7 @@
 import React , {useState,useEffect} from "react" ;
-import './Potter.css'
+import './potter.css'
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import {Link} from 'react-router-dom';
-
-
-
-// function Characters(){
-//     return(
-//         <>
-//         <div>characters</div>
-//         </>
-//     )
-
-// }
-
-
-
-// export function Potter(){
-
-//     return(
-//         <>
-//         <div className="Potter">
-//             <div className="navpotter">
-//                 <button >characters</button>
-//                 <button>Books</button>
-//                 <button>Spells</button>
-//                 <button>houses</button>
-//             </div>
-
-
-//         </div>
-//         <Router>
-//             <Routes>
-//                 <Route path="/potter/character" element={<Characters/>} ></Route>
-//             </Routes>
-//         </Router>
-//         </>
-
-        
-
-
-//     )
-
-
-
-// }
-
-
-
-
 
 
 export const Potter = () => {
